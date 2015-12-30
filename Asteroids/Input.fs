2edf1b1c419907cf4917e202ee2b0289e22ee4e2
Bob van Schendel <bobyantius@hotmail.com>
2015-12-30 11:17:50 +0100

    made small changes, about to start messing with some stuff so a commit as a backup

--- Asteroids/Input.fs
+++ Asteroids/Input.fs
@@ -9,23 +9,8 @@ type MouseInput =
   | MiddleButton = 1
   | RightButton  = 2
 
-type InputAction =
-  | KBinput of Keys
-  | Minput of MouseInput
-
-type InputReaction<'a> = 'a -> 'a
-
-//A conversion from an action(input) to an output(reaction)
-type InputBehavior<'a> = Map<InputAction, InputReaction<'a>>
-
-(*we get an inputbehavior type with a certain number of entries that map an action to a reaction
-so we need to check all the keypresses that occured and return a list of matching reactions
-*)
-let checkFunction (behavior: Map<InputAction, InputReaction<'a>>) (obj:'a) key =
-  match behavior.TryFind key with
-  | Some reaction -> reaction obj
-  | None -> obj
-
+//A conversion from an action(key) to an output(transformation action)
+type InputBehavior<'a> = Map<Keys, 'a -> 'a>
 
 let checkRightButton mouselist =
   if Mouse.GetState().LeftButton = ButtonState.Pressed then
@@ -45,10 +30,9 @@ let checkLeftButton () =
   else
     checkMiddleButton []
 
-let ProcessInput (behavior:InputBehavior<'a>) (obj:'a) =
-  let KBi = List.ofArray <| Keyboard.GetState().GetPressedKeys()
-  let Mi = checkLeftButton()
-  let KBi' = List.fold (fun state elem -> (KBinput(elem)::state)) List<InputAction>.Empty KBi
-  let Mi' = List.fold (fun state elem -> (Minput(elem)::state)) List<InputAction>.Empty Mi
-  let obj' = List.fold (checkFunction behavior) obj KBi'
-  List.fold (checkFunction behavior) obj Mi'
+let ProcessInput (beh : InputBehavior<'a>) =
+  fun elem ->
+  let pressedKeys = Keyboard.GetState().GetPressedKeys() |> List.ofArray
+  List.fold (fun elem key ->  match (beh.TryFind <| key) with
+                              | Some func -> func elem
+                              | None -> elem) elem pressedKeys
