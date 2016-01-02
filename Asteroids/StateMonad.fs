7c88fcab2699f418f267a7cbb7153dd810986f66
Bob van Schendel <bobyantius@hotmail.com>
2016-01-02 13:47:36 +0100

    updated the Cstatemonad to only include functions we need.

--- Asteroids/StateMonad.fs
+++ Asteroids/StateMonad.fs
@@ -17,16 +17,8 @@ let rec (>>=) (p:Cstate<'a, 's>) (k:'a -> Cstate<'b, 's>) : Cstate<'b, 's> =
 
 let ret a = fun s -> Done(a, s)
 
-let Second c =
-  fun s ->
-    match c s with
-    | Done(a,s) -> s
-    | Yield(a,s) -> failwith "something went wrong"
-
 type CstateBuilder () =
   member this.Return (a : 'a) = ret a
   member this.ReturnFrom (c : Cstate<'a, 's>) : Cstate<'a, 's> = c
-  member this.Snd c = Second c
-  member this.Zero () = fun s -> Done((), s)
   member this.Bind (p : Cstate<'a, 's>, k : 'a -> Cstate<'b, 's>) = p >>= k
 let cs = CstateBuilder()
